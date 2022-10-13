7d71ae2e84fe69fe3b069c5fc39accaab3625ac3
29td <nonhlanhlamndebele9@gmail.com>
2022-10-13 15:21:21 +0200

    write fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -9,4 +9,24 @@ class Solver
       return result 
     end
   end
-  
+
+  def reverse(string)
+    string.reverse
+  end
+
+  def fizzbuzz(num)
+    result = ''
+    (1..num).each do |i|
+      result += if (i % 15).zero?
+                  'fizzbuzz,'
+                elsif (i % 3).zero? && i % 5 != 0
+                  'fizz,'
+                elsif i % 3 != 0 && (i % 5).zero?
+                  'buzz,'
+                else
+                  "#{i},"
+                end
+    end
+    result
+  end
+end
